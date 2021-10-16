e54ddef8b9f80400efb573d5c34a7ec00f02483a
Michael Hauptman <44419515+mhaupt63@users.noreply.github.com>
2021-10-16 13:25:49 +1100

    Add files via upload

--- ChangePointDetector.py
+++ ChangePointDetector.py
@@ -6,12 +6,23 @@ Created on Tue Oct  5 17:56:11 2021
 
 CHANGE POINT DETECTOR
 
-This module takes a time series and returns:
+This module takes a time series and returns the uunderlying linear trend and changpoints. It uses EVT as described in https://www.robots.ox.ac.uk/~sjrob/Pubs/LeeRoberts_EVT.pdf
 
-the underlaying linear trend
-the times where there is a change in the time series, either a one-off spike or change in underlying trend
+INSTRUCTIONS:
 
-It uses EVT as described in https://www.robots.ox.ac.uk/~sjrob/Pubs/LeeRoberts_EVT.pdf
+1. from ChangePointDetector import ChangePointDetector 
+2. Prepare your time series as data plus Panda dates
+3. Create  the necessary Kalman representation by creating a "session" object by calling the ChangePoint class, e.g.:
+	Session=ChangePointDetector.ChangePointDetectorSession(data,dates). 'SeasonalityPeriods' is an optional input, e.g 12 = calendar month seasonality
+4. Determine the changepoints by running the ChangePointDetectorFunction on your "session", e.g.
+	Results=Session.ChangePointDetectorFunction()
+5. This will return a "Results" object that contains the following:
+	- ChangePoints.  This is a list of 0s and 1s the length of the data, where 1s represent changepoints
+	- Prediction.  This is the Kalman smoothed actuals, plus a 3 period forecast.  Note no forecast will be made if there is a changepoint in the last 3 		dates
+	- PredictionVariance
+	- ExtendedDates.  These are the original dates plus 3 exta for the forecast (if a forecast has been made)
+	- Trend.  This is the linear change factor
+	- TrendVariance.  Variance of the trend
 
 
 """
@@ -155,7 +166,8 @@ class MyKalmanFilter:
         self.K_gain=K_gain
         
 
-class ChangePointDetector:
+   
+class ChangePointDetectorSession:
     def __init__(self, data,dates, SeasonalityPeriods=1):
         self.ts=TimeSeriesData(data,dates)
         self.TimeseriesModel = MyKalmanFilter(self.ts)
